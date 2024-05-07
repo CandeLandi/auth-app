070aac3aca748d1902675a64fba2d27749bdb375
Candela Landi <landicandela01@gmail.com>
2024-05-07 17:28:53 -0600

    first commit

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    RegisterPageComponent,
+    LoginPageComponent,
+    AuthLayoutComponent,
+    
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    RouterModule
+  ]
+})
+export class AuthModule { }
---
+++ src/app/dashboard/layouts/dashboar-layout/dashboar-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './dashboar-layout.component.html',
+  styleUrl: './dashboar-layout.component.css'
+})
+export class DashboarLayoutComponent {
+
+}
